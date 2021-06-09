4f3a9d5c4c1e009b05f2da52f6386582d1ad81ec
Parvana-jk <64589836+Parvana-jk@users.noreply.github.com>
2021-06-09 17:33:37 +0530

    case 4

--- WebDriverTest/src/test/java/example/NewTest.java
+++ WebDriverTest/src/test/java/example/NewTest.java
@@ -41,6 +41,19 @@ public class NewTest {
             Assert.assertTrue(title.contains("Parvana's Homepage"));
         
         }
+		@Test
+		public void testEasy3() {
+            driver.get("https://auems1.netlify.app/");
+            String title = driver.getTitle();
+            driver.findElement(By.id ("email")).sendKeys("cb.en.u4cse18042@cb.students.amrita.edu");   
+    	    
+    	    driver.findElement(By.id ("pwd")).sendKeys("login1") ;
+    	    
+    	    driver.findElement(By.id("Button")).submit();  
+    	    title = driver.getTitle();
+            Assert.assertTrue(title.contains("Login"));
+        
+        }
 		
 		@BeforeTest
 		public void beforeTest() {	
