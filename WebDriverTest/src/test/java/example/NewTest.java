d932a42dc879c3d93b507ef7a93a1832862be162
Parvana-jk <64589836+Parvana-jk@users.noreply.github.com>
2021-06-10 16:18:12 +0530

    10 test cases

--- WebDriverTest/src/test/java/example/NewTest.java
+++ WebDriverTest/src/test/java/example/NewTest.java
@@ -1,10 +1,13 @@
 package example;		
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;		
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;		
 import org.testng.annotations.Test;
 
@@ -14,12 +17,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;		
 public class NewTest {		
 	    private WebDriver driver;		
-		@Test				
-		public void testEasy() {	
-			driver.get("http://demo.guru99.com/test/guru99home/");  
-			String title = driver.getTitle();				 
-			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
-		}	
+		
 		@Test
 		public void testEasy1() {
             driver.get("https://auems1.netlify.app/");
@@ -112,59 +110,155 @@ public class NewTest {
 		@Test
 		public void testEasy5() {
             driver.get("https://auems1.netlify.app/");
+           
             String title = driver.getTitle();
-            driver.findElement(By.id ("email")).sendKeys("hariharan@cb.amrita.edu");   
-            try {
-				Thread.sleep(4000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    	    driver.findElement(By.id ("pwd")).sendKeys("login") ;
-    	    try {
-				Thread.sleep(4000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    	    
-    	    driver.findElement(By.id("Button")).submit();  
-    	    try {
-				Thread.sleep(4000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    	    title = driver.getTitle();
-            Assert.assertTrue(title.contains("Department Dashboard"));
+    	 
+            Assert.assertTrue(title.contains("Admin Dashboard"));
         
         }
 		@Test
 		public void testEasy6() {
-            driver.get("https://auems1.netlify.app/");
+            driver.get("https://auems1.netlify.app/loginform");
+            driver.findElement(By.id("back-link")).click();
+            try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
             String title = driver.getTitle();
-            driver.findElement(By.id ("email")).sendKeys("john@cb.admin.amrita.edu");   
-    	    
-    	    driver.findElement(By.id ("pwd")).sendKeys("login1") ;
-    	    
-    	    driver.findElement(By.id("Button")).submit();  
-    	    title = driver.getTitle();
-            Assert.assertTrue(title.contains("Login"));
+    	 
+            Assert.assertTrue(title.contains("Admin Dashboard"));
         
         }
 		@Test
 		public void testEasy7() {
-            driver.get("https://auems1.netlify.app/");
+            driver.get("https://auems1.netlify.app/deptdashboard");
+            driver.get("https://auems1.netlify.app/loginform");
+            driver.findElement(By.id("back-link")).click();
+            try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
             String title = driver.getTitle();
-            driver.findElement(By.id ("email")).sendKeys("hariharan@cb.admin.amrita.edu");   
-    	    
-    	    driver.findElement(By.id ("pwd")).sendKeys("login1") ;
-    	    
-    	    driver.findElement(By.id("Button")).submit();  
-    	    title = driver.getTitle();
+    	 
+            Assert.assertTrue(title.contains("Admin Dashboard"));
+        
+        }
+		@Test
+		public void testEasy8() {
+			driver.findElement(By.id(("dropdown"))).click();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			driver.findElement(By.id(("logout"))).click();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			 driver.findElement(By.id ("email")).sendKeys("hariharan@cb.amrita.edu");   
+			 try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	    	    driver.findElement(By.id ("pwd")).sendKeys("login") ;
+	    	    try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	    	    driver.findElement(By.id("Button")).submit();  
+	    	    try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	    	    String title = driver.getTitle();
+            Assert.assertTrue(title.contains("Department Dashboard"));
+        
+        }
+		
+		@Test
+		public void testEasy9() {
+			driver.findElement(By.id(("dropdown"))).click();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			driver.findElement(By.id(("logout"))).click();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			 driver.findElement(By.id ("email")).sendKeys("hariharan@cb.amrita.edu");   
+			 try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	    	    driver.findElement(By.id ("pwd")).sendKeys("login1") ;
+	    	    try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	    	    driver.findElement(By.id("Button")).submit();  
+	    	    try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	    	    String title = driver.getTitle();
             Assert.assertTrue(title.contains("Login"));
         
         }
+		@Test
+		public void testEasy10() {
+			
+			 driver.findElement(By.id ("email")).sendKeys("john@cb.admin.amrita.edu");   
+			 try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	    	    driver.findElement(By.id ("pwd")).sendKeys("login1") ;
+	    	    try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	    	    driver.findElement(By.id("Button")).submit();  
+	    	    try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	    	    String title = driver.getTitle();
+            Assert.assertTrue(title.contains("Login"));
+        
+        }
+		
 		
 		
 		@BeforeTest
